c17fdeacff88606791b748c164015bb686120256
hberumen <hberumen@uaz.edu.mx>
2016-05-10 18:40:35 -0500

    First

---
+++ MyFirstPlayground.playground/Contents.swift
@@ -0,0 +1,8 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "My first, playground"
+var hora: String = "Mi primer variable"
+var saludo = "hola"
+var saludo = 3
