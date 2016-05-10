ec22836e660c09fbc3888fc728ddca9ff86568b8
hberumen <hberumen@uaz.edu.mx>
2016-05-10 18:46:55 -0500

    Constantes.

--- MyFirstPlayground.playground/Contents.swift
+++ MyFirstPlayground.playground/Contents.swift
@@ -5,4 +5,14 @@ import UIKit
 var str = "My first, playground"
 var hora: String = "Mi primer variable"
 var saludo = "hola"
-var saludo = 3
+
+// Constantes
+
+let salidoExt: String = "Holar first"
+var numero: Int = 7
+let decimal: Double = 5.0
+
+let mySaludo = "Sin declaracion"
+
+numero = 3
+
